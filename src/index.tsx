171c6e70ccf14288a8da0ccd1944ab527b681c08
Constantine <barr.develop@gmail.com>
2020-10-24 21:04:31 +0400

    add redux persistor

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./screens";
 import { Provider } from "react-redux";
-import appStore from "./store/";
+import appStore, { persistor } from "./store/";
 import { configureApi, ApiProvider } from "./api";
+import { PersistGate } from "redux-persist/integration/react";
 import * as serviceWorker from "./serviceWorker";
 
 const api = configureApi(appStore);
@@ -12,9 +13,11 @@ const api = configureApi(appStore);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={appStore}>
-      <ApiProvider api={api}>
-        <App />
-      </ApiProvider>
+      <PersistGate persistor={persistor} loading={null}>
+        <ApiProvider api={api}>
+          <App />
+        </ApiProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
